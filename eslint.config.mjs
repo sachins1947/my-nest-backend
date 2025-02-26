c6b58bb891384f53618edad262ee62527bf9598c
sachins1947 <sachin1947s@gmail.com>
2025-02-26 11:59:36 +0530

    used husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,7 +31,7 @@ export default tseslint.config(
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unused-vars': [
-        'warn',
+        'error',
         { argsIgnorePattern: '^_' },
       ],
     },
